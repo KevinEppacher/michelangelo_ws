30f2d87fa3868b4384421f6b129e0f6f900512fc
kevin <kevin-eppacher@hotmail.de>
2023-11-03 00:24:23 +0100

    end of 3.11

--- src/main.cpp
+++ src/main.cpp
@@ -8,21 +8,69 @@
 #include <string.h>     /* for memset() */
 #include <unistd.h>     /* for close() */
 
+#define RCVBUFSIZE 32   /* Size of receive buffer */
 
-int main()
+void DieWithError(char *errorMessage)
 {
-    Robot::MobileRobot mobot;
+    perror(errorMessage);
+    exit(1);
+}
 
-    // IP-Adresse und Port des Servers (Raspberry Pi)
-    const char* serverIP = "192.168.100.52";
-    const int serverPort = 9999;
+int main(int argc, char *argv[])
+{
+    int sock;                        /* Socket descriptor */
+    struct sockaddr_in echoServAddr; /* Echo server address */
+    unsigned short echoServPort;     /* Echo server port */
+    char *servIP;                    /* Server IP address (dotted quad) */
+    char *echoString;                /* String to send to echo server */
+    char echoBuffer[RCVBUFSIZE];     /* Buffer for echo string */
+    unsigned int echoStringLen;      /* Length of string to echo */
+    int bytesRcvd, totalBytesRcvd;   /* Bytes read in single recv() 
+                                        and total bytes read */
 
-    // Erzeugen eines Socket
-    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (clientSocket == -1) {
-        std::cerr << "Fehler beim Erzeugen des Sockets" << std::endl;
-        return 1;
+    if ((argc < 3) || (argc > 4))    /* Test for correct number of arguments */
+    {
+       fprintf(stderr, "Usage: %s <Server IP> <Echo Word> [<Echo Port>]\n",
+              argv[0]);
+       exit(1);
     }
+
+    
+    servIP = argv[1];             /* First arg: server IP address (dotted quad) */
+    echoString = argv[2];         /* Second arg: string to echo */
+
+    if (argc == 4)
+        echoServPort = atoi(argv[3]); /* Use given port, if any */
+    else
+        echoServPort = 7;  /* 7 is the well-known port for the echo service */
+
+
+    /* Construct the server address structure */
+    memset(&echoServAddr, 0, sizeof(echoServAddr));     /* Zero out structure */
+    echoServAddr.sin_family      = AF_INET;             /* Internet address family */
+    echoServAddr.sin_addr.s_addr = inet_addr(servIP);   /* Server IP address */
+    echoServAddr.sin_port        = htons(echoServPort); /* Server port */
+
+    echoStringLen = strlen(echoString);          /* Determine input length */
+
+    /* Receive the same string back from the server */
+    totalBytesRcvd = 0;
+    printf("Received: ");                /* Setup to print the echoed string */
+    while (totalBytesRcvd < echoStringLen)
+    {
+        /* Receive up to the buffer size (minus 1 to leave space for
+           a null terminator) bytes from the sender */
+        if ((bytesRcvd = recv(sock, echoBuffer, RCVBUFSIZE - 1, 0)) <= 0)
+            DieWithError("recv() failed or connection closed prematurely");
+        totalBytesRcvd += bytesRcvd;   /* Keep tally of total bytes */
+        echoBuffer[bytesRcvd] = '\0';  /* Terminate the string! */
+        printf("%s", echoBuffer);      /* Print the echo buffer */
+    }
+
+    printf("\n");    /* Print a final linefeed */
+
+    close(sock);
+    exit(0);
 
     
     return 0;
