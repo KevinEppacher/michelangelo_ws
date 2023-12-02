3f2e076fccbd1b8eb849c28fbe70509b6aeac1fa
kevin <kevin-eppacher@hotmail.de>
2023-12-02 12:27:23 +0100

    command odom erfolgreich

--- src/Robot.h
+++ src/Robot.h
@@ -55,7 +55,56 @@ namespace Robot
 
     };
 
+    class TCPClient {
+    public:
+        TCPClient(const char* serverIP, int port) {
+            client_fd = socket(AF_INET, SOCK_STREAM, 0);
+            if (client_fd < 0) {
+                printf("\n Socket creation error \n");
+                exit(EXIT_FAILURE);
+            }
 
+            serv_addr.sin_family = AF_INET;
+            serv_addr.sin_port = htons(port);
+
+            if (inet_pton(AF_INET, serverIP, &serv_addr.sin_addr) <= 0) {
+                printf("\nInvalid address/ Address not supported \n");
+                exit(EXIT_FAILURE);
+            }
+
+            if (connect(client_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+                printf("\nConnection Failed \n");
+                exit(EXIT_FAILURE);
+            }
+            }
+
+            ~TCPClient() {
+                std::cout<<"Client wurde geschlossen111"<<std::endl;
+                close(client_fd);
+            }
+
+            void closeTCPconnection()
+            {
+                close(client_fd);
+            }
+
+            void sendData(const char* data) {
+                send(client_fd, data, strlen(data), 0);
+                std::cout << "Message sent: " << data << std::endl;
+                close(client_fd);
+            }
+
+            void receiveData(char* buffer, ssize_t size) {
+                valread = read(client_fd, buffer, size - 1);
+                buffer[valread] = '\0'; // Null-terminator hinzufügen
+                std::cout << buffer << std::endl;
+            }
+
+            private:
+                int client_fd;
+                ssize_t valread;
+                struct sockaddr_in serv_addr;
+        };
 }
 
 #endif // ROBOT_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,88 @@
 #include "Robot.h"
+#include <iostream>
+#include <sstream> 
 
+int main(int argc, char *argv[]) {
+
+    char buffer[1024] = {0};
+
+    //Robot::TCPClient client("127.0.0.1", 8080);
+        double i = 0;
+
+    while(true)
+    {
+        i+=0.005;  
+        double linear  = 0 + i;
+        double angular  = 0;
+        std::stringstream ss;
+        ss << "---START---{\"linear\": " << linear << ", \"angular\": " << angular << "}___END___";
+        std::string echoString = ss.str();
+
+        std::cout<<i<<std::endl;
+
+        Robot::TCPClient client("192.168.100.55", 9999);
+        client.sendData(echoString.c_str());
+        client.receiveData(buffer, sizeof(buffer));  
+        //client.closeTCPconnection();
+        
+
+    }
+
+    
+
+    return 0;
+}
+
+/*
+#pragma once
+#include "tcpHandler.h"
+class sendCmd : private TCPHandler
+{
+private:
+public:
+    sendCmd(){};
+    ~sendCmd(){};
+    void sendCmdVel(float linear, float angular)
+    {
+        std::cout << "sendCmdVel is called!\n";
+        echoServPort = 9999;
+        std::stringstream ss;
+        ss << "---START---{\"linear\":" << linear << ", \"angular\":" << angular << "}__END__";
+        std::string echoString = ss.str();
+
+        if ((sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+            DieWithError("socket() failed");
+        memset(&echoServAddr, 0, sizeof(echoServAddr));
+        echoServAddr.sin_family = AF_INET;
+        echoServAddr.sin_addr.s_addr = inet_addr(servIP.c_str());
+        echoServAddr.sin_port = htons(echoServPort);
+        if (connect(sock, (struct sockaddr *)&echoServAddr, sizeof(echoServAddr)) < 0)
+        {
+            DieWithError("connect() failed");
+        }
+
+        size_t echoStringLen = echoString.length();
+
+        if (send(sock, echoString.c_str(), echoStringLen, 0) != static_cast<ssize_t>(echoStringLen))
+            DieWithError("send() sent a different number of bytes than expected");
+        std::cout << std::endl;
+
+        close(sock);
+    }
+};
+
+
+*/
+
+
+
+
+
+
+
+
+
+/*
 int main(int argc, char *argv[])
 {
     //Robot::Socket robotSocket(argv[1], argv[2], (argc == 4) ? atoi(argv[3]) : 7);
@@ -9,7 +92,6 @@ int main(int argc, char *argv[])
 
 
     // Receive the same string back from the server
-    std::cout << "Received: "; /* Setup to print the echoed string */
     while (true)
     {
         robotSocket.sendAndReceiveData();
@@ -17,3 +99,5 @@ int main(int argc, char *argv[])
     }
     return 0;
 }
+
+*/
