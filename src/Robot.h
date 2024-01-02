7efe4d73719eb604286b22367457443b36ca3298
CoCoKayya18 <collins1999.ck@gmail.com>
2024-01-02 09:30:09 +0100

    Json-Handler is jz mit kevins code gefused, plus cmake wurde umgeändert, sodass die json library bei jedem gleich mitdabei ist. Testnungen wären not bad.

--- src/Robot.h
+++ src/Robot.h
@@ -1,51 +1,120 @@
-//#ifndef ROBOT_H
+#ifndef ROBOT_H
 #define ROBOT_H
 
 #include <iostream>
 #include <cstring>
-#include <cstdlib>
 #include <unistd.h>
-#include <arpa/inet.h>
 #include <sys/socket.h>
+#include <arpa/inet.h> 
+#include <netinet/in.h>
 #include <nlohmann/json.hpp>
+#include <Eigen/Geometry> 
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include <chrono>
+
+
 #define RCVBUFSIZE 100000   /* Size of receive buffer */
 
 namespace Robot
 {
     struct Pose
     {
+        int index = 0;
+        double tolerance = 0;
         struct Position
         {
             double x = 0, y = 0, z = 0;
-            double vx = 0, vy = 0, vz = 0; // Geschwindigkeiten
         };
 
         struct Orientation
         {
-            double roll = 0, pitch = 0, yaw = 0;
-            double vRoll = 0, vPitch = 0, vYaw = 0; // Winkelgeschwindigkeiten
+            double x = 0, y = 0, z = 0, w = 0;
         };
 
         Position position;
         Orientation orientation;
     };
+
+
+    struct Twist
+    {
+        struct Linear
+        {
+            double x = 0, y = 0, z = 0; // Geschwindigkeiten
+        };
+
+        struct Angular
+        {
+            double x = 0, y = 0, z = 0, w = 0; // Winkelgeschwindigkeiten
+        };
+
+        Linear linear;
+        Angular angular;
+    };
+
+    struct Parameter
+    {
+        double P = 0.1, I = 0, D = 0;
+
+        double prevDerivative = 0;
+        double prevError = 0;
+        double error;
+
+        double proportionalError = 0;
+        double integralError = 0;
+        double derivativeError = 0;
+    };
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+MobileRobot
+*/
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+     
     
 
+  
     class MobileRobot
     {
     public:
         MobileRobot();
         ~MobileRobot();
-
+        void publishCmdVel(double* linear_x, double* angular_z);
         bool linearController(Robot::Pose goalPose, Robot::Pose currentPose);
+        bool pidController(Twist* cmdVel, Parameter PID, double totalDistance, double alpha, double beta);
+        bool limitControllerVariables(Twist* cmdVel, double upperLimit, double lowerLimit);
+        bool convertQuaternionsToEuler(Pose* currentAngle);
+        int goTo(Pose* goalPose, Pose* currentPose);
+        bool run();
+
+    protected:
         double calculateTotalDistance(Robot::Pose diffPose);
+        double calculateGamma(Robot::Pose diffPose);
+        double calculateAlpha(double gamma, Robot::Pose currentAngle);
+        double calculateBeta(Robot::Pose goalPose, double gamma);
+        bool calculateRobotVektor();
+        bool calculateVektorFromRobotToGoal();
+        double calculateYaw(Pose* qA);
+        double calculateRoll(Pose* qA);
+        double calculatePitch(Pose* qA);
+        double angleDiff(double angle1, double angle2);
+        bool orientationController(Robot::Pose goalPose, Robot::Pose currentPose);
+        Robot::Pose robotPose;
 
     private:
-        double scanMsg;
-        double odomMsg;
-        double cmdVelMsg;
         Robot::Pose diffPose;
-        const double totalDistance = 0;
+        Robot::Twist cmdVel;
+        int sequenceNumber = 1;
+        float robotVector[2] = { 0 , 0 };
+        float distanceVector[2] = { 0 , 0 };
+        double totalDistance = 0;
+        double gamma = 0;
+        double alpha = 0;
+        double beta = 0;
+        double dt = 0;
     };
 
 
@@ -60,9 +129,7 @@ namespace Robot
 
         void sendData(const char* data);
 
-        std::string receiveData(char* buffer, ssize_t size);
-
-        Robot::Pose getOdom(std::string receivedData);
+        void receiveData(char* buffer, ssize_t size);
 
     private:
         int client_fd;
@@ -95,3 +162,5 @@ namespace Robot
 
 
 }
+
+#endif // ROBOT_H
