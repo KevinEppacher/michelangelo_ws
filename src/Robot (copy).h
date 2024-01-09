5f60fb3ff914cb60a8704057b71ec591d3e484fb
JulianH <haringjulian@gmail.com>
2024-01-09 16:38:05 +0100

    modified:   src/Robot.cpp 	modified:   src/Robot.h

--- src/Robot (copy).h
+++ src/Robot (copy).h
@@ -9,22 +9,10 @@
 #include <arpa/inet.h>
 #include <signal.h>
 #include <sys/socket.h>
-<<<<<<< Updated upstream
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
-=======
-#include <arpa/inet.h> 
-#include <netinet/in.h>
-#include <nlohmann/json.hpp>
-#include <Eigen/Geometry> 
-#include <SFML/Graphics.hpp>
-#include <vector>
-#include <chrono>
-#include <stdlib.h> 
-
->>>>>>> Stashed changes
 
 #define RCVBUFSIZE 100000   /* Size of receive buffer */
 
@@ -35,31 +23,6 @@ namespace Robot
     public:
         MobileRobot();
         ~MobileRobot();
-<<<<<<< Updated upstream
-=======
-        void publishCmdVel(double* linear_x, double* angular_z);
-        bool linearController(Robot::Pose goalPose, Robot::Pose currentPose);
-        bool pidController(Twist* cmdVel, Parameter PID, double totalDistance, double alpha, double beta);
-        bool limitControllerVariables(Twist* cmdVel, double upperLimit, double lowerLimit);
-        bool convertQuaternionsToEuler(Pose* currentAngle);
-        int goTo(Pose* goalPose, Pose* currentPose);
-        bool run();
-
-
-    protected:
-        double calculateTotalDistance(Robot::Pose diffPose);
-        double calculateGamma(Robot::Pose diffPose);
-        double calculateAlpha(double gamma, Robot::Pose currentAngle);
-        double calculateBeta(Robot::Pose goalPose, double gamma);
-        bool calculateRobotVektor();
-        bool calculateVektorFromRobotToGoal();
-        double calculateYaw(Pose* qA);
-        double calculateRoll(Pose* qA);
-        double calculatePitch(Pose* qA);
-        double angleDiff(double angle1, double angle2);
-        bool orientationController(Robot::Pose goalPose, Robot::Pose currentPose);
-        Robot::Pose robotPose;
->>>>>>> Stashed changes
 
     private:
         double scanMsg;
@@ -117,31 +80,6 @@ namespace Robot
 
     };
 
-<<<<<<< Updated upstream
-=======
-    class SharedMemories
-    {
-        public:
-        void producerHandler (int sig);  // Signal handler for the producer
-        void consumerHandler (int sig);  // Signal handler for the consumer
-
-        struct Message           // Format of the messages
-        {
-            int type;            // message type
-            int data;             // content of the message
-        };
-        enum MessageType { PROD_MSG=1, CONS_MSG };
-        //used to differentiate bewteen the sender of the message
-
-        int msgqid;                // identifier of the message queue
-        pid_t child_pid;           // identifier of the forked process
-        
-
-
-    }
-    //COCO//
-
->>>>>>> Stashed changes
 
 }
 
