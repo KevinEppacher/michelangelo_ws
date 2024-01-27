7b5be23e41ee9787bb904dd6d40d77cf2506ae81
kevin <kevin-eppacher@hotmail.de>
2024-01-27 23:38:13 +0100

    main bissi geändert

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,6 @@ int main(int argc, char **argv)
     Robot::MobileRobot turtle(argv[1]);
     Robot::Visualizer visualizer;
 
-    // Receive the same string back from the server
-    std::cout << "Received: "; /* Setup to print the echoed string */
     while (true)
     {
         //turtle.run();
