7aae2aa8c54eea07304a74a2d6a6537b0cdd52f5
Collins Kayyalakkakam <91327892+CoCoKayya18@users.noreply.github.com>
2024-01-30 15:39:30 +0100

    Updated Robot.h

--- src/Robot.h
+++ src/Robot.h
@@ -27,8 +27,19 @@
 
 #define RCVBUFSIZE 100000   /* Size of receive buffer */
 
+// Define the Robot namespace to encapsulate all related classes and functions
 namespace Robot
 {
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+Struct definitions
+*/
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+   
+   
+    // Pose struct represents the position and orientation of the robot
     struct Pose
     {
         int index = 0;
@@ -47,11 +58,12 @@ namespace Robot
         Orientation orientation;
     };
 
+    // Convert degrees to radians
     double convertDegreesToRadiant(double degrees);
 
 
 
-
+    // Twist struct represents the velocity of the robot
     struct Twist
     {
         struct Linear
@@ -68,6 +80,9 @@ namespace Robot
         Angular angular;
     };
 
+
+
+    // Parameter struct for PID controller parameters
     struct Parameter
     {
         double P = 0.1, I = 0, D = 0;
@@ -81,6 +96,9 @@ namespace Robot
         double derivativeError = 0;
     };
 
+
+
+    // Circle struct for representing circular paths
     struct Circle
     {
         double xOffset = 0;
@@ -92,25 +110,34 @@ namespace Robot
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /*
+Struct definitions
+*/
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
 MobileRobot
 */
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
      
     
 
-  
+    // MobileRobot class handles the robot's movement and control
     class MobileRobot
     {
     public:
         MobileRobot(char* ip);
         MobileRobot(){};
         ~MobileRobot();
+
         void publishCmdVel(double* linear_x, double* angular_z);
-        bool linearController(Robot::Pose goalPose, Robot::Pose currentPose);
-        bool pidController(Twist* cmdVel, Parameter PID, double totalDistance, double alpha, double beta);
+        bool linearController(Robot::Pose goalPose, Robot::Pose currentPose, bool wall);
+        bool pidController(Twist* cmdVel, Parameter PID, double totalDistance, double alpha, double beta, bool wall);
         bool limitControllerVariables(Twist* cmdVel, double upperLimit, double lowerLimit);
         bool convertQuaternionsToEuler(Pose* currentAngle);
-        int goTo(Pose* goalPose, Pose* currentPose);
+        int goTo(Pose* goalPose, Pose* currentPose, bool wall);
         bool run();
         void setIP(char* ipAdress);
 
@@ -129,8 +156,8 @@ MobileRobot
         bool orientationController(Robot::Pose goalPose, Robot::Pose currentPose);
         Robot::Pose robotPose;
         void arrivedEndgoal();
-        void process(std::string odomData);
-        std::string receive();
+        void process(std::string odomData, std::string laserscanData);
+        std::string receive(int port);
 
     private:
         char* ip;
@@ -144,45 +171,42 @@ MobileRobot
         double alpha = 0;
         double beta = 0;
         double dt = 0;
+        Eigen::VectorXd PCA_Angles;
         std::chrono::high_resolution_clock::time_point time;
         std::chrono::high_resolution_clock::time_point lastTime;
         long long getTimeMS();
     };
 
 
+
+    // TCPClient class for handling TCP/IP communication
     class TCPClient: public MobileRobot
     {
     public:
         TCPClient(const char* serverIP, int port);
         TCPClient(){};
-        
         ~TCPClient();
 
         void closeTCPconnection();
-
         void sendData(const char* data);
-
         std::string receiveData(char* buffer, ssize_t size);
 
     private:
         int client_fd;
         ssize_t valread;
         struct sockaddr_in serv_addr;
-        
-
     };
-
-    //COCO//
-
 
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /*
-TCPServer
+TCPServer (for testing without Turtlebot)
 */
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  
+
+
+
     class TCPServer {
     public:
         TCPServer(int port);
@@ -200,6 +224,25 @@ TCPServer
         int port;
     };
 
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+TCPServer
+*/
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+Json Parser
+*/
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+    //Json class for parsing Sensor-Data
     class JsonHandler
     {
         public:
@@ -219,36 +262,98 @@ TCPServer
 
 
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+Json Parser
+*/
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+PCA Class
+*/
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+    //Class, which handles PCA calculation
+    class PCA
+    {
+        public:
+            PCA();
+            ~PCA();
+
+            void runPCA(std::vector<double> rawLaserScan, int ScanSample);  
+            Eigen::MatrixXd PolarToCartesian(Eigen::VectorXd laserScanData);
+            void FilterLaserscan(Eigen::MatrixXd laserScanData, int filterTolerance);
+            Eigen::VectorXd computePCA(const Eigen::MatrixXd &data);
+            void plotData(Eigen::MatrixXd laserScanData, Eigen::VectorXd PCA_vector, Eigen::VectorXd PCA_vectorRight, int filterTolerance);
+            Eigen::VectorXd getAngleDifference();
+            Eigen::MatrixXd getFilteredLeftScanData();
+            Eigen::MatrixXd getFilteredRightScanData();
+            Eigen::VectorXd getPCA_Left();
+            Eigen::VectorXd getPCA_Right();
+
+        private:
+            Eigen::MatrixXd filteredLaserScanLeftSide;
+            Eigen::MatrixXd filteredLaserScanRightSide;
+            Eigen::VectorXd PCA_Left;
+            Eigen::VectorXd PCA_Right;
+
+    };
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+PCA Class
+*/
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /*
 shared Memory
 */
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-     
-class SHM {
-public:
-    SHM(const std::string& input);
-    ~SHM();
-    
-    std::string returnOutput();
-    int processID;
-    bool shutdown = false;
 
-private:
-    struct SHM_Message {
-        char information[16000];
+
+
+    //SHM class for shared memory management
+    class SHM {
+        public:
+            SHM(const std::string& input);
+            ~SHM();
+            
+            std::string returnOutput();
+            int processID;
+            bool shutdown = false;
+
+        private:
+            struct SHM_Message {char information[16000];};
+            void checkSignal(int semid);
+            void setSignal(int semid);
+            static void signalHandler(int sig, SHM* instance);
+
+            int mutexID;
+            int shmID;
+            SHM_Message* shmptr;
+            std::string input;
+            std::string output;
     };
 
-    void checkSignal(int semid);
-    void setSignal(int semid);
-    static void signalHandler(int sig, SHM* instance);
 
-    int mutexID;
-    int shmID;
-    SHM_Message* shmptr;
 
-    std::string input;
-    std::string output;
-};
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+shared Memory
+*/
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
 }
 #endif // ROBOT_H
